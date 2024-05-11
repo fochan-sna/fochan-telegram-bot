d7961fcdc77301a0ec0b7fde71b3ad15cea8a828
anijackich <anijackich@gmail.com>
2024-05-11 03:34:52 +0300

    Fix main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
 import asyncio
 import logging
-import sys
 from os import getenv
 from dotenv import load_dotenv
 
@@ -20,20 +20,6 @@ dp = Dispatcher()
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
-
-
-@dp.message()
-async def echo_handler(message: types.Message) -> None:
-    report = leak_osint.request([message.text])[0]
-
-    await message.answer('\n\n'.join(
-        f'<b>{db_name}</b>\n' + '\n'.join(
-            '\n'.join(
-                [f'├ {field} - {value}' for field, value in list(block.items())[:-1]] +
-                [f'└ {list(block.keys())[-1]} - {list(block.values())[-1]}']
-            ) for block in data['Data']
-        ) for db_name, data in report.items()
-    ))
 
 
 async def main() -> None:
