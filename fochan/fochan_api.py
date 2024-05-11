c912aa296637f3b8227999e1a19f667d237e8a43
anijackich <anijackich@gmail.com>
2024-05-11 07:47:03 +0300

    Add logic

--- fochan/fochan_api.py
+++ fochan/fochan_api.py
@@ -1,17 +1,30 @@
 from requests import Session
 
 from .types import *
+from .const import *
 
 
 class FochanAPI:
     def __init__(self):
         self.http = Session()
 
+    def create_user(self) -> User:
+        return User(
+            **self.http.get(FOCHAN_API_CREATE_USER_URL).json()
+        )
+
     def get_topics(self) -> list[Topic]:
-        pass
+        return [Topic(**topic)
+                for topic in self.http.get(FOCHAN_API_GET_TOPICS_URL).json()]
 
-    def get_messages(self, topic_id: TopicID, limit: int = 3):
-        pass
+    def get_messages(self, topic_id: TopicID, limit: int = 3) -> list[Message]:
+        return [Message(**message)
+                for message in self.http.get(FOCHAN_API_GET_MESSAGES_URL,
+                                             params={'topic_id': topic_id,
+                                                     'limit': limit}).json()]
 
-    def send_message(self, topic_id: TopicID, user_id: UserID, message: str):
-        pass
+    def send_message(self, topic_id: TopicID, user_id: UserID, message: str) -> None:
+        self.http.post(FOCHAN_API_SEND_MESSAGE_URL,
+                       json={'topic_id': topic_id,
+                             user_id: user_id,
+                             message: message})
--- main.py
+++ main.py
@@ -6,9 +6,11 @@ from dotenv import load_dotenv
 
 from aiogram import Bot, Dispatcher, Router, types
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
-from aiogram.utils.markdown import hbold
+from aiogram.utils.markdown import hbold, hcode
+
+from fochan import FochanAPI
 
 load_dotenv()
 
@@ -16,10 +18,66 @@ TELEGRAM_BOT_TOKEN = getenv("TELEGRAM_BOT_TOKEN")
 
 dp = Dispatcher()
 
+fochan = FochanAPI()
+
+users = {}
+
 
 @dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
+async def start(message: Message) -> None:
+    if message.from_user.id not in users:
+        users[message.chat.id] = {
+            'user': fochan.create_user(),
+            'current_topic': None
+        }
+
     await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
+
+
+@dp.message(Command('topics'))
+async def list_topics(message: Message) -> None:
+    await message.answer('\n\n'.join(
+        f'{hbold(topic.name)} {hcode(topic.id[-4:])}\n'
+        f'{topic.description}'
+        for topic in fochan.get_topics()
+    ))
+
+
+@dp.message(Command('topic'))
+async def enter_topic(message: Message) -> None:
+    topic_alias = message.text.split()[1]
+    users[message.chat.id]['current_topic'] = next(
+        (topic for topic in fochan.get_topics()
+         if topic.id[-4:] == topic_alias), None
+    )
+
+    if users[message.chat.id]['current_topic']:
+        await message.answer(
+            f'You have entered to '
+            f'{hbold(users[message.chat.id]['current_topic'].name)} topic'
+        )
+        return
+
+    await message.answer('Topic not found')
+
+
+@dp.message(Command('exit'))
+async def leave_topic(message: Message) -> None:
+    users[message.chat.id]['current_topic'] = None
+    await message.answer('You have exited from the topic')
+
+
+@dp.message()
+async def send_message_or_default(message: Message) -> None:
+    if not users[message.chat.id]['current_topic']:
+        await message.answer('Enter the topic to start messaging')
+        return
+
+    fochan.send_message(
+        topic_id=users[message.chat.id]['current_topic'].id,
+        user_id=users[message.chat.id]['user'].id,
+        message=message.text
+    )
 
 
 async def main() -> None:
